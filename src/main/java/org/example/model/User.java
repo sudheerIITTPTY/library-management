4ef2176c8a8ffc4cc9e9cca8810634d438a9c01e
sudheerIITTPTY <cs19m005@iittp.ac.in>
2024-11-29 02:37:10 +0530

    adding more properties to model class

---
+++ src/main/java/org/example/model/User.java
@@ -0,0 +1,68 @@
+package org.example.model;
+
+public class User {
+    private int userId;
+    private String userName;
+    private String password;
+    private String membershipStatus;
+    private String pincode;
+    private String city;
+    private int fineAmount;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getMembershipStatus() {
+        return membershipStatus;
+    }
+
+    public void setMembershipStatus(String membershipStatus) {
+        this.membershipStatus = membershipStatus;
+    }
+
+    public String getPincode() {
+        return pincode;
+    }
+
+    public void setPincode(String pincode) {
+        this.pincode = pincode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getFineAmount() {
+        return fineAmount;
+    }
+
+    public void setFineAmount(int fineAmount) {
+        this.fineAmount = fineAmount;
+    }
+
+}
